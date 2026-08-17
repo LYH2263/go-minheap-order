7d6ab8a8ae02fc8292d8ef527e88f87128cf5447
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-17 08:42:16 +0800

    fix: apply test model repair

--- internal/pq/pq.go
+++ internal/pq/pq.go
@@ -4,7 +4,7 @@ package pq
 type IntHeap []int
 
 func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool  { return h[i] > h[j] } // BUG
+func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] } // min-heap
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 func (h *IntHeap) Push(x interface{})  { *h = append(*h, x.(int)) }
 func (h *IntHeap) Pop() interface{} {
